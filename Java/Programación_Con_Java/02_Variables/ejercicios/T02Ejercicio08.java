0ac6cda4abbf90c420a133c632860bf2a6bde8d6
TheRealGGIOVI <ggiomes590@g.educaand.es>
2023-10-27 15:48:38 +0200

    Todos los ejercicios del tema 02 hechos

---
+++ Java/Programación_Con_Java/02_Variables/ejercicios/T02Ejercicio08.java
@@ -0,0 +1,15 @@
+/**
+ * Explica tu código aquí
+ * 
+ * @author Giovanni Giove
+*/
+public class T02Ejercicio08 {
+  public static void main(String[] args) {
+    char letra1 = 'P';
+    char letra2 = 'E';
+    char letra3 = 'R';
+    char letra4 = 'R';
+    char letra5 = 'O';
+    System.out.println("" +letra1 +letra2 +letra3 +letra4 +letra5);
+  }
+}

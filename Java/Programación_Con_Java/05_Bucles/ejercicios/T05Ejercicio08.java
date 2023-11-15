af29cd25ab3cc4a8358ac618790857052a361481
TheRealGGIOVI <ggiomes590@g.educaand.es>
2023-11-15 18:01:22 +0100

    ejercicio 8 hecho

--- Java/Programación_Con_Java/05_Bucles/ejercicios/T05Ejercicio08.java
+++ Java/Programación_Con_Java/05_Bucles/ejercicios/T05Ejercicio08.java
@@ -8,6 +8,17 @@ import java.util.Scanner;
 public class T05Ejercicio08 {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int num;
+
+    System.out.print("Introduce un número del 1 al 10: ");
+    num = sc.nextInt();
+
     
+    for(int j = 0; j <= 10; j++){
+      System.out.println(num + " * " +j + " = " +num*j);
+    }
+    
+
+    sc.close();
   }
 }
